975868aff3a38648212aa406b633efe8d51b331e
Juan Bertelegni <juanbertelegni@gmail.com>
2025-11-26 19:23:07 -0300

    Actualización del Proyecto Final

--- src/components/Carrito.jsx
+++ src/components/Carrito.jsx
@@ -2,26 +2,28 @@
 // Componente Carrito
 // ==================
 
-// El componente recibe est 'carrito' (array-productos) y func 'vaciarCarrito' como props.
-function Carrito({carrito, vaciarCarrito}) {
-    // Renderizado condicional: Si length(array)=0 --> está vacío, muestra un msj y termina ejecución del componenete (no renderiza el resto).
+function Carrito({ carrito, vaciarCarrito }) {
+    // Renderizado condicional
     if (carrito.length === 0) return <p>El carrito está vacío</p>;
 
     return (
-        <div style = {{
-            border: "1px solid #ccc",
-            padding: "10px",
-            margin: "20px",
-            borderRadius: "10px"}}
+        <div
+            style={{
+                border: "1px solid #ccc",
+                padding: "10px",
+                margin: "20px",
+                borderRadius: "10px",
+            }}
         >
             <h3>Carrito</h3>
-            {/* Itera sobre array 'carrito' p/mostrar c/ítem. */}
-            {/* key={i} es p/indetif c/elem de la lista. i-índice se utiliza si no hay ID único. */}
+
             {carrito.map((item, i) => (
-                <p key = {i}>{item.title}-${item.price}</p>
+                <p key={i}>
+                    {item.nombre} – ${item.precio}
+                </p>
             ))}
-            {/* Si click, llama a prop 'vaciarCarrito', que ejecuta func actualización de est en componente padre (App). */}
-            <button onClick = {vaciarCarrito}>Vaciar carrito</button>
+
+            <button onClick={vaciarCarrito}>Vaciar carrito</button>
         </div>
     );
 }
--- src/components/ProductoDetalle.jsx
+++ src/components/ProductoDetalle.jsx
@@ -1,6 +1,6 @@
 // Importa 'useParams' de 'react-router-dom' p/leer parámes de URL.
 import { useParams } from "react-router-dom";
-import { useState, useEffect } from "react";
+import { useProducts } from "react";
 
 // ==========================
 // Componente ProductoDetalle
@@ -11,53 +11,30 @@ function ProductoDetalle(){
     const {id} = useParams();
     // EST DEL PRODUCTO:
     // Inicializa 'producto'=null, ya que componente comienza s/datos del producto.
-    const [producto, setProducto] = useState(null);
-    // EST DE CARGA:
-    // Inicializa 'cargando'=true, indicando que la solicitud a API está en curso.
-    const [cargando, setCargando] = useState(true);
-    // EST DE ERROR:
-    // P/manejar + mostrar cualquier error ocurrido durante fetch.
-    const [error, setError] = useState(null);
+    const {productos} = useProducts();
+    
+    const producto = productos.find((p) => p.id === id);
 
-    // useEffect: Ejecuta lógica p/obtener producto de la API.
-    useEffect(() => {
-        // Fetch a la API: Se utiliza (` `) p/incrustar el 'id' obtenido de la URL, creando ruta dinám p/producto específico.
-        fetch(`https://www.fakestoreapi.com/products/${id}`)
-            .then(respuesta=>respuesta.json())
-            .then(datos => {
-                // Si solicitud=OK, actualiza est 'producto'.
-                setProducto(datos);
-                // Desactiva el estado de carga.
-                setCargando(false);
-            })
-            .catch(error=>{
-                console.error("Error al obtener el producto:", error);
-                // Guarda msj error + desactiva la carga.
-                setError("No se pudo obtener el producto.");
-                setCargando(false);
-            });
-            // Array de dependencias contiene [id]. Fetch se ejecutará c/vez que val 'id' cambie.
-    }, [id]);
+    if (!producto) {
+        return <p>Producto no encontrado.</p>;
+    }
 
-    // RENDERIZADO CONDICIONAL p/manejar 3 est posibles:
-    // Si cargando:
-    if (cargando) return <p>Cargando producto...</p>;
-    // Si ocurrió error (ej. problema red o 404 personalizado):
-    if (error) return <p>{error}</p>;
-    // Si no hay producto (ej. API=null o API=objeto vacío y no fue manejado por error):
-    if (!producto) return <p>Producto no encontrado.</p>;
+    return (
+        <div style={{ textAlign: "center", marginTop: "20px" }}>
+            <h2>{producto.nombre}</h2>
 
-    // RENDERIZADO FINAL: Muestra detalles de producto si todo OK.
-    return(
-        <div style={{textAlign: "center", marginTop: "20px"}}>
-            <h2>{producto.title}</h2>
+        {producto.imagen && (
             <img
-                src={producto.image}
-                alt={producto.title}
-                style={{width: "200px", height: "200px"}}
+                src={producto.imagen}
+                alt={producto.nombre}
+                style={{ width: "200px", height: "200px" }}
             />
-            <p>{producto.description}</p>
-            <p><strong>Precio:</strong> ${producto.price}</p>
+        )}
+
+        <p>{producto.descripcion}</p>
+        <p>
+            <strong>Precio:</strong> ${producto.precio}
+        </p>
         </div>
     );
 }
